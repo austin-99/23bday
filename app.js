31fbd9b25239d2dc5dd6dba1bf484ced07cfd060
austin-99 <78673041+austin-99@users.noreply.github.com>
2022-11-27 10:54:14 -0500

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love you for showing me what being loved feels like. You are the first person who made me feel so cared for and loved baby";
+        message.innerHTML = "I love how you have the most beautiful eyes in the world";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
