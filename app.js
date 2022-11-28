6a4e42754bf9f9e10b19f5c744fdcaec4ebedbbf
austin-99 <78673041+austin-99@users.noreply.github.com>
2022-11-28 09:49:25 -0500

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how you have the most beautiful eyes in the world";
+        message.innerHTML = "I love when you drew goldy pong from The Promised Neverland for me. It made my heart go crazy to have my baby take her time to draw me something I love so much";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
