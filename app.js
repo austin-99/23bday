9f364ee1e777d97f9946f9422b59f31f94cfae72
austin-99 <78673041+austin-99@users.noreply.github.com>
2022-12-03 10:51:57 -0500

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love you for being the only one who makes my heart so warm, happy and complete";
+        message.innerHTML = "I love the way you study when you would talk to yourself and you need to take breaks and go downstairs to talk to your mom. I think that’s so cute baby";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
