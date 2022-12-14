38ccd251954975b2f99338429febe27a7db355d8
austin-99 <78673041+austin-99@users.noreply.github.com>
2022-12-14 10:20:48 -0500

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how invested you are in me baby. You always do more than enough to make me feel better";
+        message.innerHTML = "I love how cute you are when you said that you have to fight the urge to not eat raw batter <br> Also, CONGRATS on finishing your semester baby I'm so happy that you are DONE and now baby can rest. <br> I am so proud of you Lana. The things you accomplish are not easy at all but you make it look easy because of now smart and talented you are";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
