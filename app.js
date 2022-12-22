7d3781b7ba01b2e555ae76ca8fdcc85b3daa1f6a
austin-99 <78673041+austin-99@users.noreply.github.com>
2022-12-22 10:32:01 -0500

    yes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = " ";
+        message.innerHTML = "I love how you are my teddy bear. I always knew that I would be so lucky to have someone whose personality trait is like a teddy bear (warm, sweet, and makes me what to cuddle you all day)";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
