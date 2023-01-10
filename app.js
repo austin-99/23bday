34a003b098856242ba059a76e3a1c6b697bc307b
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-01-10 09:05:06 -0500

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how you feel like home to me because you make my heart feel so warm and safe";
+        message.innerHTML = "I love it when you go to sleep with me because it makes me feel so complete";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
