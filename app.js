647652032f4b9ad4087c3af1bb83b784132683d9
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-01-13 08:33:01 -0500

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how you always wish me things on holidays right at 12:00am. You are always the most thoughtful person in the world";
+        message.innerHTML = "I love how you make everyday so memorable. I love all the memories we make together";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
