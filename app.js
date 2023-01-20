1cf581a2b31f3816753542d9e784e8b129004575
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-01-20 08:33:08 -0500

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how you love to go hiking and I want to go hiking with you on our first date";
+        message.innerHTML = "I love how you put your favourite emoji on your AirPods once. It’s one of the cutest things you’ve done";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
