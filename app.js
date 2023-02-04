a29bc595fbde0ed5fa3a0c12b4415302ee5b563a
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-02-04 10:54:08 -0500

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how you always tell me that you want to cuddle me every time we go to sleep baby it makes me feel so happy because I want to cuddle you so badly when I’m with you";
+        message.innerHTML = "I love the way you want to sit on my lap and then ride me hard baby. I love it when you tell me these things and I can’t wait to do it with you";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
