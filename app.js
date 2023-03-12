40355800149d171e73fa35762d97c46a204468d2
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-03-12 11:09:44 -0400

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how you make the talking stage so memorable and innocent<br>I love how you always give me hope in everything<br>I love how we get to always learn from each other<br>I love how you loved my hood days and that it made you laugh really hard";
+        message.innerHTML = "I love how much you make me love your grey leggings";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
