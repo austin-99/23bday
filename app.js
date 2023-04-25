18d9ae9906214c617da4ee4b8629c9d0b8ff4b23
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-04-25 08:01:29 -0400

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love the way you say good night to me and how you want to cuddle and hold me baby";
+        message.innerHTML = "I love you for wanting to be mine baby because you mean everything to me";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
