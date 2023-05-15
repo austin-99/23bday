24b5521f1f01acc311ea42d87d46fc799937ddf4
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-05-15 07:52:43 -0400

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how passionate you are when it comes to loving me";
+        message.innerHTML = "I love how you make my heart feel so whole and complete";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
