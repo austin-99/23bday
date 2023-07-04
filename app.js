8c8139e91219fcdcf44845db0b00044c1528509c
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-07-04 07:58:29 -0400

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love you for always wanting to be there for my sister";
+        message.innerHTML = "I love how affectionate you are and it makes me feel special how you are only affectionate to me";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
