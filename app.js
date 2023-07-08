6e4d8d38c784efba0e0cf2dce0413586f250fc66
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-07-08 10:52:25 -0400

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love how my baby will teach me Arabic so I can flirt with you better";
+        message.innerHTML = "I love how my baby will teach me Arabic. I’ll speak it like it’s my first language";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
