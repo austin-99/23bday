17f730293c4a2e823f49451f183e309cecda579c
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-09-01 12:50:08 -0400

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love everything about your personality and I know for a fact I can’t find that anywhere else";
+        message.innerHTML = "I love how you make every holiday feel so special because I get to spend it with you";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
