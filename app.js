dc1bccfc4b2f2cc73aaec159e538b5724db96e98
austin-99 <78673041+austin-99@users.noreply.github.com>
2023-09-04 22:19:47 -0400

    u

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function myFunction(){
     let message = document.querySelector("#message");
 
     if (userInput.value == "yes" || userInput.value == "Yes" || userInput.value == "YES"){
-        message.innerHTML = "I love all the pooh gifs we send to each other because you are my pooh that I want to hold";
+        message.innerHTML = "I love you for always praying for me and it means so much that you do";
     }
     else if (userInput.value == "no" || userInput.value == "No"){
         message.innerHTML = "please say yes baby it will make you happier";
